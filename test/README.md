8a263db2768d91281106dd556f0c6154a038fa7f
dann254 <dann.professional@gmail.com>
2017-08-29 22:09:22 +0300

    DEPLOY - first deploy attempt

---
+++ test/README.md
@@ -0,0 +1,5 @@
+# TEST
+this are the test used on the program.
+
+## Test 1.
+  this app verifies if when password and confirm password are similar, the result is TRUE. 
--- test/app_test.py
+++ test/app_test.py
@@ -2,6 +2,7 @@ import unittest
 from app.user_management import UserRegister
 
 class password_test(unittest.TestCase):
+    #test 1
     def test_simmilar_passwords_return_true(self):
         result = UserRegister().verify_password_similarity("monkey", "monkey")
         self.assertEqual(True, result)
