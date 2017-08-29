881559830b657bc5dc2390fc5537eee882b5eca4
dann254 <dann.professional@gmail.com>
2017-08-29 15:56:07 +0300

    FEATURE - password verification and test for signup

--- test/app_test.py
+++ test/app_test.py
@@ -1,4 +1,10 @@
 import unittest
+from app.user_management import UserRegister
+
+class password_test(unittest.TestCase):
+    def test_simmilar_passwords_return_true(self):
+        result = UserRegister().verify_password_similarity("monkey", "monkey")
+        self.assertEqual(True, result)
 
 if __name__ == '__main__':
     unittest.main()
