47328ca2a8199de6361093f1658343d4bfdc231e
dann254 <dann.professional@gmail.com>
2017-08-30 00:10:25 +0300

    UPDATE - readme

--- runserver.py
+++ runserver.py
@@ -1,3 +1,6 @@
 # Run a test server.
 from app import app
+#running the app on other server e.g heroku
 app.run
+# running the app locally in debug mode
+#app.run(host='127.0.0.1', port=5000, debug=True)
