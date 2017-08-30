2de38dd76285eeaa7fedd3eac429a27aa2b116f1
dann254 <dann.professional@gmail.com>
2017-08-30 11:56:13 +0300

    UPDATE- handling and testing user registration

--- test/app_test.py
+++ test/app_test.py
@@ -11,16 +11,38 @@ class RegisterTest(unittest.TestCase):
     def test_different_passwords_return_false(self):
         result = UserRegister().verify_password_similarity("monkey", "baboon")
         self.assertEqual(False, result)
-
     #test 3
-    def blank_check_success(self):
-        result = UserRegister().check_for_blanks("name", "mail@mail","passord","cpassword")
+    def username_success_test(self):
+        result = UserRegister().check_if_username_exists("user1")
         self.assertEqual(True, result)
-
     #test 4
-    def blank_check_fail(self):
-        result = UserRegister().check_for_blanks("name", "mail@mail","cpassword")
+    def username_fail_test(self):
+        result = UserRegister().check_if_username_exists("user")
         self.assertEqual(False, result)
+    #test 5
+    def email_success_test(self):
+        result = UserRegister().check_if_email_exists("user1@mail")
+        self.assertEqual(True, result)
+    #test 6
+    def email_fail_test(self):
+        result = UserRegister().check_if_email_exists("user@mail")
+        self.assertEqual(False, result)
+    #test 7
+    def blanks_success_test(self):
+        result = UserRegister().check_for_blanks("name", "user1@mail", "password", "cpassword")
+        self.assertEqual(True, result)
+    #test 8
+    def blanks_fail_test(self):
+        result = UserRegister().check_for_blanks("", "user1@mail", "", "cpassword")
+        self.assertEqual(False, result)
+    #test 9
+    def sucessful_register_test(self):
+        result = FormSubmission().after_user_submit_register("name", "user1@mail", "password", "password")
+        self.assertEqual("success", result)
+    #test 9
+    def faild_register_test(self):
+        result = FormSubmission().after_user_submit_register("", "user1@mail", "password", "password")
+        self.assertFalse("success"==result)
 
 if __name__ == '__main__':
     unittest.main()
